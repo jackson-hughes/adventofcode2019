61da33e777fd2d80c0ee7065af612d2ca7671e8f
Jackson Hughes <jackson.hughes@infinityworks.com>
2019-12-03 12:17:10 +0000

    Add functional 02 solution

--- 02/main.go
+++ 02/main.go
@@ -1,26 +1,73 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"math"
+	"os"
+	"strconv"
 )
 
-func calculateFuelsFuel(fuel float64) (totalFuel float64) {
-	d := fuel / float64(3)
-	r := math.Floor(d)
-	s := r - float64(2)
+func calculateFuel(moduleMass float64) (fuelRequirement int, err error) {
+	d := moduleMass / float64(3)
+	s := int(d) - 2
+	return s, nil
+}
+
+func calculateFuelsFuel(fuel int) (totalFuel int) {
+	d := fuel / 3
+	s := d - 2
+	// fmt.Println(s)
 	if s <= 0 {
 		return totalFuel
 	}
-	// fmt.Println(s)
 	return s + calculateFuelsFuel(s)
 }
 
+func readModules() (modules []float64, err error) {
+	file, err := os.Open("modules.txt")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	s := []float64{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if module, err := strconv.ParseFloat(scanner.Text(), 64); err == nil {
+			s = append(s, module)
+		} else {
+			return nil, err
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
 func main() {
-	moduleFuel := 3246455
+	var totalFuelRequirement int
 
-	fuelFuel := calculateFuelsFuel(float64(moduleFuel))
+	modules, err := readModules()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	totalFuel := float64(moduleFuel) + fuelFuel
-	fmt.Println(totalFuel)
+	s := []int{}
+	for _, x := range modules {
+		if y, err := calculateFuel(x); err == nil {
+			z := calculateFuelsFuel(y)
+			s = append(s, y, z)
+		} else {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	for _, x := range s {
+		totalFuelRequirement += x
+	}
+	fmt.Println(totalFuelRequirement)
 }
